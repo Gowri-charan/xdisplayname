20b597422808ab124e1af0c23c2714db633afb1e
gowricharan <gowricharan16@gmail.com>
2024-07-17 20:38:15 +0530

    2

---
+++ src/test.jsx
@@ -0,0 +1,50 @@
+import React, {useState } from "react";
+function App() {
+
+  const [data1, setData1] = useState(null);
+  const [data2, setData2] = useState(null);
+  // const [data, setData] = useEffect(null);?\
+  const [show, SetShow] = useState(false);
+  
+  function getData1(val1){
+    setData1(val1.target.value)
+    SetShow(false)
+  }
+  function getData2(val2){
+    setData2(val2.target.value)
+    SetShow(false)
+  }
+
+  return (
+    <div className="App">
+      <form>
+        <h2>Full Name Display</h2>
+        <label for="name">First Name:</label>
+        <input type="text" id="fname" onChange={getData1} required/>
+        <br/> <br/>
+        <label for="name">First Name:</label>
+        <input type="text" id="lname" onChange={getData2} required/>
+        <br/>        <br/>
+        <button 
+          type="submit" 
+          onClick={(e) => {
+            e.preventDefault();
+            if( data1 && data2) {
+                SetShow(true);
+            } else{
+                alert("Please fill First & last name")
+            }
+        }}>
+          Submit
+        </button>
+      </form>
+      {
+        show? 
+        <p><label>Full Name: </label>{data1 +" " + data2}</p>
+        :null
+      }
+    </div>
+  );
+}
+
+export default App;
